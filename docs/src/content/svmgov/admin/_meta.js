41d93294cb2b31cf2d02113b24edb394aad3be96
Micaiah Reid <micaiahreid@gmail.com>
2026-06-05 14:38:42 -0400

    docs: update docs to reflect svmgov config management commands

---
+++ docs/src/content/svmgov/admin/_meta.js
@@ -0,0 +1,17 @@
+export default {
+  index: {
+    title: 'Overview',
+  },
+  'init-global-config': {
+    title: 'Initialize Global Config',
+  },
+  'update-global-config': {
+    title: 'Update Global Config',
+  },
+  'show-global-config': {
+    title: 'Show Global Config',
+  },
+  'transfer-admin': {
+    title: 'Transfer Admin',
+  },
+};
